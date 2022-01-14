69d63650cccf6f3e06027d9c4b42a5fc0b4a1167
JorgeP <jalexispoveda010@gmail.com>
2022-01-14 07:47:33 -0500

    Configurando enzyme, probando el primer componente

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,13 +1,22 @@
-import PrimeraApp from "../PrimeraApp";
 import { render } from "@testing-library/react";
+
 import React from "react";
+import { shallow } from "enzyme";
+import PrimeraApp from "../PrimeraApp";
 
 describe("Probando primeraApp component", () => {
-  test("debe mostrar el mensaje hola soy goku", () => {
-    const saludo = "Hola soy goku";
-    //Producto renderizado de lo que yo voy a probar
-    const wrapper = render(<PrimeraApp saludo={saludo}></PrimeraApp>);
+  //   test("debe mostrar el mensaje hola soy goku", () => {
+  //     const saludo = "Hola soy goku";
+  //     //Producto renderizado de lo que yo voy a probar
+  //     const wrapper = render(<PrimeraApp saludo={saludo}></PrimeraApp>);
+  //     expect(wrapper.getByText(saludo)).toBeInTheDocument();
+  //   });
 
-    expect(wrapper.getByText(saludo)).toBeInTheDocument();
+  test("debe mostrar <primeraApp> component correctamente ", () => {
+    const saludo = "Hola soy goku";
+    const wrapper = shallow(<PrimeraApp saludo={saludo}></PrimeraApp>);
+    //probando que el componente se muestr en pantalla correctamente
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
